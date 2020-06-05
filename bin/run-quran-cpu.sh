005150448b32ce49df193026879dbe4ca20748b2
omerasif-itu <mscs16039@itu.edu.pk>
2020-06-05 19:17:07 +0500

    add script run-quran.sh for cpu training
    to traind on cpu machine

---
+++ bin/run-quran-cpu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -xe
+if [ ! -f DeepSpeech.py ]; then
+    echo "Please make sure you run this from DeepSpeech's top level directory."
+    exit 1
+fi;
+
+if [ ! -d "${COMPUTE_DATA_DIR}" ]; then
+    COMPUTE_DATA_DIR="data/quran"
+fi;
+
+# Warn if we can't find the train files
+if [ ! -f "${COMPUTE_DATA_DIR}/quran_train.csv" ]; then
+    echo "Warning: It looks like you don't have the Quran corpus downloaded"\
+         "and preprocessed. Make sure \$COMPUTE_DATA_DIR points to the folder where"\
+         "the Quran data is located, and that you ran the" \
+         "importer script at bin/import_quran.py before running this script."
+fi;
+
+python3 -u DeepSpeech.py \
+  --train_files "$COMPUTE_DATA_DIR/quran_train.csv" \
+  --dev_files "$COMPUTE_DATA_DIR/quran_dev.csv" \
+  --test_files "$COMPUTE_DATA_DIR/quran_test.csv" \
+  --alphabet_config_path "$COMPUTE_DATA_DIR/quran-alphabets.txt" \
+  --scorer "$COMPUTE_DATA_DIR/lm/quran.scorer" \
+  --export_dir "/data/model" \
+  --train_batch_size 32 \
+  --dev_batch_size 32 \
+  --test_batch_size 32 \
+  --use_allow_growth "true" \
+  --noearly_stop \
+  --epochs 32 \
+  --export_language "ar" \
+  --n_hidden 2048 \
+  --dropout_rate 0.5 \
+  --learning_rate 0.0001 \
+  --checkpoint_dir "data/ckpts" \
+  --max_to_keep 2 \ 
+  "$@"
+
